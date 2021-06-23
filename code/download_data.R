ee11ab3782750fa9028f0e2916641788f181602d
Josh Archer <jearcher@berkeley.edu>
2021-06-23 13:42:49 -0700

    updating from last time

--- code/download_data.R
+++ code/download_data.R
@@ -122,14 +122,167 @@ ca_acs <- readRDS("../data/census/ca_acs.rds")
 unemp_vars <- c("B23025_003E","B23025_004E","B23025_005E")
 unemp_years <- c(2016:2019)
 
+ca_unemp <- map(
+  unemp_years,
+  ~ get_acs(geography = "tract",
+            variables = unemp_vars,
+            year = .x,
+            state = "06",
+            output = "wide")
+  ) %>% 
+  map2(unemp_years, ~ mutate(.x, id = .y)) 
 
-# there is also a file called "../census/census_wide_ca.csv" which has a bunch of census data (96 vars)
-# Only includes 2000, 2009, 2010, 2012, 2018
-# ca_census <- fread("../census/census_wide_ca.csv")
+ca_unemp <- (reduce(ca_unemp, rbind)) 
 
-# ACS Data
+ca_unemp <- ca_unemp %>%
+  rename(year = id,
+         labor_force = B23025_003E,
+         employed = B23025_004E,
+         unemployed = B23025_005E,
+         tract = NAME) %>%
+  mutate(unemp_rate = unemployed/labor_force) %>%
+  select(GEOID, tract, unemp_rate, year)
+
+ca_unemp %>% count(year)
+
+saveRDS(ca_unemp, file = "../data/unemployment/ca_acs_unemp.rds")
+
+# ACS Data ----
+
+# I want to download all the ACS data at once
+# Some of this is adapted from HPRM
+
+##### This is HPRM code, slight adjustments to the variables desired
+acs_vars = c(
+  'Total' = 'B25003_001',
+  'Rent' = 'B25003_003',
+  'Total_WHITE' = 'B25003A_001',
+  'Rent_WHITE' = 'B25003A_003',
+  'Total_BLACK' = 'B25003B_001',
+  'Rent_BLACK' = 'B25003B_003',
+  'Total_AI' = 'B25003C_001',
+  'Rent_AI' = 'B25003C_003',
+  'Total_ASIAN' = 'B25003D_001',
+  'Rent_ASIAN' = 'B25003D_003',
+  'Total_NHPI' = 'B25003E_001',
+  'Rent_NHPI' = 'B25003E_003',
+  'Total_OTHER' = 'B25003F_001',
+  'Rent_OTHER' = 'B25003F_003',
+  'Total_TWO' = 'B25003G_001',
+  'Rent_TWO' = 'B25003G_003',
+  'Total_WHITE_NL' = 'B25003H_001',
+  'Rent_WHITE_NL' = 'B25003H_003',
+  'Total_LATINX' = 'B25003I_001',
+  'Rent_LATINX' = 'B25003I_003',
+  'MedRent' = 'B25064_001',
+  'MHHInc' = 'B19013_001',
+  'rb_tot' = 'B25070_001', # GROSS RENT AS A PERCENTAGE OF HOUSEHOLD INCOME
+  'rb_10.0' = 'B25070_002', # Less than 10.0 percent GROSS RENT as % hh income
+  'rb_14.9' = 'B25070_003', # 10.0 to 14.9 percent GROSS RENT as % hh income
+  'rb_19.9' = 'B25070_004', # 15.0 to 19.9 percent GROSS RENT as % hh income
+  'rb_24.9' = 'B25070_005', # 20.0 to 24.9 percent GROSS RENT as % hh income
+  'rb_29.9' = 'B25070_006', # 25.0 to 29.9 percent GROSS RENT as % hh income
+  'rb_34.9' = 'B25070_007', # 30.0 to 34.9 percent GROSS RENT as % hh income
+  'rb_39.9' = 'B25070_008', # 35.0 to 39.9 percent GROSS RENT as % hh income
+  'rb_49.9' = 'B25070_009', # 40.0 to 49.9 percent GROSS RENT as % hh income
+  'rb_55' = 'B25070_010', # 50.0 percent or more GROSS RENT as % hh income
+  'rb_nc' = 'B25070_011', # Not computed GROSS RENT as % hh income)
+  'totrace' = 'B03002_001',
+  'White' = 'B03002_003',
+  'Black' = 'B03002_004',
+  'Asian' = 'B03002_006',
+  'Latinx' = 'B03002_012',
+  'totwelf' = 'B19057_001', #
+  'welf' = 'B19057_002',
+  'totpov' = 'B17017_001', #
+  'povfamh' = 'B17017_003',
+  'povnonfamh' = 'B17017_020', 
+  'totenroll' = 'B14007_001', # Estimate!!Total SCHOOL ENROLLMENT BY DETAILED  LEVEL OF SCHOOL FOR THE POPULATION 3 YEARS AND OVER
+  'colenroll' = 'B14007_017', # Estimate!!Total!!Enrolled in school!!Enrolled in college undergraduate years  SCHOOL ENROLLMENT BY DETAILED  LEVEL OF SCHOOL FOR THE POPULATION 3 YEARS AND OVER
+  'proenroll' = 'B14007_018', # Estimate!!Total!!Enrolled in school!!Graduate or professional school  SCHOOL ENROLLMENT BY DETAILED  LEVEL OF SCHOOL FOR THE POPULATION 3 
+  'HHInc_Total' = 'B19001_001', # Total HOUSEHOLD INCOME
+  'HHInc_10' = 'B19001_002', # Less than $10,000 HOUSEHOLD INCOME
+  'HHInc_15' = 'B19001_003', # $10,000 to $14,999 HOUSEHOLD INCOME
+  'HHInc_20' = 'B19001_004', # $15,000 to $19,999 HOUSEHOLD INCOME
+  'HHInc_25' = 'B19001_005', # $20,000 to $24,999 HOUSEHOLD INCOME
+  'HHInc_30' = 'B19001_006', # $25,000 to $29,999 HOUSEHOLD INCOME
+  'HHInc_35' = 'B19001_007', # $30,000 to $34,999 HOUSEHOLD INCOME
+  'HHInc_40' = 'B19001_008', # $35,000 to $39,999 HOUSEHOLD INCOME
+  'HHInc_45' = 'B19001_009', # $40,000 to $44,999 HOUSEHOLD INCOME
+  'HHInc_50' = 'B19001_010', # $45,000 to $49,999 HOUSEHOLD INCOME
+  'HHInc_60' = 'B19001_011', # $50,000 to $59,999 HOUSEHOLD INCOME
+  'HHInc_75' = 'B19001_012', # $60,000 to $74,999 HOUSEHOLD INCOME
+  'HHInc_100' = 'B19001_013', # $75,000 to $99,999 HOUSEHOLD INCOME
+  'HHInc_125' = 'B19001_014', # $100,000 to $124,999 HOUSEHOLD INCOME
+  'HHInc_150' = 'B19001_015', # $125,000 to $149,999 HOUSEHOLD INCOME
+  'HHInc_200' = 'B19001_016', # $150,000 to $199,999 HOUSEHOLD INCOME
+  'HHInc_250' = 'B19001_017', # $200,000 or more HOUSEHOLD INCOME
+  'HHIncTen_Total' = 'B25118_001', # Total
+  'HHIncTenOwn' = 'B25118_002', # Owner occupied
+  'HHIncTenOwn_5' = 'B25118_003', # Owner occupied!!Less than $5,000
+  'HHIncTenOwn_10' = 'B25118_004', # Owner occupied!!$5,000 to $9,999
+  'HHIncTenOwn_15' = 'B25118_005', # Owner occupied!!$10,000 to $14,999
+  'HHIncTenOwn_20' = 'B25118_006', # Owner occupied!!$15,000 to $19,999
+  'HHIncTenOwn_25' = 'B25118_007', # Owner occupied!!$20,000 to $24,999
+  'HHIncTenOwn_35' = 'B25118_008', # Owner occupied!!$25,000 to $34,999
+  'HHIncTenOwn_50' = 'B25118_009', # Owner occupied!!$35,000 to $49,999
+  'HHIncTenOwn_75' = 'B25118_010', # Owner occupied!!$50,000 to $74,999
+  'HHIncTenOwn_100' = 'B25118_011', # Owner occupied!!$75,000 to $99,999
+  'HHIncTenOwn_150' = 'B25118_012', # Owner occupied!!$100,000 to $149,999
+  'HHIncTenOwn_151' = 'B25118_013', # Owner occupied!!$150,000 or more
+  'HHIncTenRent' = 'B25118_014', # Renter occupied
+  'HHIncTenRent_5' = 'B25118_015', # Renter occupied!!Less than $5,000
+  'HHIncTenRent_10' = 'B25118_016', # Renter occupied!!$5,000 to $9,999
+  'HHIncTenRent_15' = 'B25118_017', # Renter occupied!!$10,000 to $14,999
+  'HHIncTenRent_20' = 'B25118_018', # Renter occupied!!$15,000 to $19,999
+  'HHIncTenRent_25' = 'B25118_019', # Renter occupied!!$20,000 to $24,999
+  'HHIncTenRent_35' = 'B25118_020', # Renter occupied!!$25,000 to $34,999
+  'HHIncTenRent_50' = 'B25118_021', # Renter occupied!!$35,000 to $49,999
+  'HHIncTenRent_75' = 'B25118_022', # Renter occupied!!$50,000 to $74,999
+  'HHIncTenRent_100' = 'B25118_023', # Renter occupied!!$75,000 to $99,999
+  'HHIncTenRent_150' = 'B25118_024', # Renter occupied!!$100,000 to $149,999
+  'HHIncTenRent_151' = 'B25118_025', # Renter occupied!!$150,000 or more
+  # Added  by project home
+  'labor_force' = 'B23025_003E',
+  'employed' = 'B23025_004E',
+  'unemployed' = 'B23025_005E'
+)
+
+acs_years <- c(2016:2019)
+
+ca_df_ph <- map(
+  acs_years,
+  ~ get_acs(geography = "tract",
+            variables = acs_vars,
+            year = .x,
+            state = "06",
+            output = "wide")
+) %>% 
+  map2(acs_years, ~ mutate(.x, id = .y)) %>% 
+  reduce(rbind)
+
+ca_df_ph <- (reduce(ca_unemp, rbind)) %>%
+  rename(year = id,
+         labor_force = B23025_003E,
+         employed = B23025_004E,
+         unemployed = B23025_005E,
+         tract = NAME) %>%
+  mutate(unemp_rate = unemployed/labor_force) %>%
+  select(GEOID, tract, unemp_rate, year)
 
 
+
+#########3
+ca_unemp <- ca_unemp %>%
+  rename(year = id,
+         labor_force = B23025_003E,
+         employed = B23025_004E,
+         unemployed = B23025_005E,
+         tract = NAME) %>%
+  mutate(unemp_rate = unemployed/labor_force) %>%
+  select(GEOID, tract, unemp_rate, year)
+
+ca_unemp %>% count(year)
 
 
 # CDC Data ----
